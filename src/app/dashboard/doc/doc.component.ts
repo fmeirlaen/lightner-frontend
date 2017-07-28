ac6225bac96ad35dfd1fde995644da0eb25f6db5
Fred <fred1337@users.noreply.github.com>
2017-07-28 07:36:30 +0200

    récuperation des données de l'api avec bon token dans le header

--- src/app/dashboard/doc/doc.component.ts
+++ src/app/dashboard/doc/doc.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { AuthenticationService } from '../../_services/index';
 
+import { TokenService } from '../../_services/index';
+
 @Component({
   selector: 'app-doc',
   templateUrl: './doc.component.html',
   styleUrls: ['./doc.component.css']
 })
 export class DocComponent  {
-  constructor(private http: Http, private authenticationService: AuthenticationService) {
-    const headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
-    const options = new RequestOptions({ headers: headers });
-    http.get(`http://localhost/lightner/web/app_dev.php/api/docs`, options)
-      .subscribe(response => {
-        console.log(response.json());
-        // logs the array of races
-      });
+  constructor(private http: Http, private tokenservice: TokenService) {
+    tokenservice.getJson('api/docs', null, 'GET').subscribe(data => {
+      console.log(data[0].contact_name);
+    });
   }
 
   OnInit() {
